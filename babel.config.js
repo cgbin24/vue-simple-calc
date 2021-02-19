5a77dcaf93a5caacfcd8c14ea71e7647eb3777b1
cgbin24 <c_being@qq.com>
2021-02-20 02:03:01 +0800

    fix: modify

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+// const path = require('path')
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  // build: {
+  //   index: path.resolve(__dirname, '../dist/index.html'),
+
+  //   assetsRoot: path.resolve(__dirname, '../dist'),
+  //   assetsSubDirectory: 'static',
+  //   assetsPublicPath: './',
+  // }
 }
--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -4,77 +4,77 @@
     <div>
       <a-row class="mb10">
         <a-col >
-          <a-input v-model="result" placeholder="请输入值" readonly style="text-align:right"/>
+          <a-input v-model="result" placeholder="请输入值" style="text-align:right"/>
         </a-col>
       </a-row>  
       <a-row class="mb10">
         <a-col :span="6">
-          <a-button @click="getVal('7')">7</a-button>
+          <a-button @click="clearVal()" class="operate">C</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="getVal('8')">8</a-button>
+          <a-button @click="back()" class="operate">&lt;=</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="getVal('9')">9</a-button>
+          <a-button @click="squareOperate()" class="operate">x<sup>2</sup></a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="clearVal()">Clear</a-button>
-        </a-col>
-      </a-row>  
-      <a-row class="mb10">
-        <a-col :span="6">
-          <a-button @click="getVal('4')">4</a-button>
-        </a-col>
-        <a-col :span="6">
-          <a-button @click="getVal('5')">5</a-button>
-        </a-col>
-        <a-col :span="6">
-          <a-button @click="getVal('6')">6</a-button>
-        </a-col>
-        <a-col :span="6">
-          <a-button @click="subtraction('-')">-</a-button>
+          <a-button @click="modOperate('%')" class="operate">%</a-button>
         </a-col>
       </a-row>  
       <a-row class="mb10">
         <a-col :span="6">
-          <a-button @click="getVal('1')">1</a-button>
+          <a-button @click="getVal('7')" class="num">7</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="getVal('2')">2</a-button>
+          <a-button @click="getVal('8')" class="num">8</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="getVal('3')">3</a-button>
+          <a-button @click="getVal('9')" class="num">9</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="addition('+')">+</a-button>
-        </a-col>
-      </a-row>  
-      <a-row class="mb10">
-        <a-col :span="6">
-          <a-button @click="getVal('0')">0</a-button>
-        </a-col>
-        <a-col :span="6">
-          <a-button @click="getVal('.')">.</a-button>
-        </a-col>
-        <a-col :span="6">
-          <a-button @click="multiplication('*')">*</a-button>
-        </a-col>
-        <a-col :span="6">
-          <a-button @click="resultVal()">=</a-button>
+          <a-button @click="addition('+')" class="operate">+</a-button>
         </a-col>
       </a-row>  
       <a-row class="mb10">
         <a-col :span="6">
-          <a-button @click="divisionOperate('÷')">÷</a-button>
+          <a-button @click="getVal('4')" class="num">4</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="squareOperate()">x<sup>2</sup></a-button>
+          <a-button @click="getVal('5')" class="num">5</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="modOperate('%')">%</a-button>
+          <a-button @click="getVal('6')" class="num">6</a-button>
         </a-col>
         <a-col :span="6">
-          <a-button @click="back()">回退</a-button>
+          <a-button @click="subtraction('-')" class="operate">-</a-button>
+        </a-col>
+      </a-row>  
+      <a-row class="mb10">
+        <a-col :span="6">
+          <a-button @click="getVal('1')" class="num">1</a-button>
+        </a-col>
+        <a-col :span="6">
+          <a-button @click="getVal('2')" class="num">2</a-button>
+        </a-col>
+        <a-col :span="6">
+          <a-button @click="getVal('3')" class="num">3</a-button>
+        </a-col>
+        <a-col :span="6">
+          <a-button @click="multiplication('*')" class="operate">*</a-button>
+        </a-col>
+      </a-row>  
+      <a-row class="mb10">
+        <a-col :span="6">
+          <a-button @click="getVal('0')" class="num">0</a-button>
+        </a-col>
+        <a-col :span="6">
+          <a-button @click="getVal('.')" class="num">.</a-button>
+        </a-col>
+        <a-col :span="6">
+          <a-button @click="resultVal()" class="operate">=</a-button>
+        </a-col>
+        <a-col :span="6">
+          <a-button @click="divisionOperate('÷')" class="operate">÷</a-button>
         </a-col>
       </a-row>  
     </div>
@@ -97,8 +97,11 @@ export default {
   methods: {
     // 获取键值
     getVal(item){
+      if (item === '.') {
+        item = '0.'
+      }
       this.newVal += item
-      this.result = this.result == 0 ? +this.newVal : this.newVal
+      this.result = this.result == 0 ? +this.newVal : this.newVal.toString()
     },
     // 清除 归零
     clearVal(){
@@ -151,10 +154,18 @@ export default {
     // 返回结果
     resultVal(){
       if (this.newVal.includes('+') || this.newVal.includes('-') || this.newVal.includes('*') || this.newVal.includes('÷') || this.newVal.includes('%')) {
+        const len = this.newVal.length
+        // 特殊值处理：判断结尾标志符号
+        const sign = this.newVal.split('')[len-1]
+        if (this.newVal.indexOf(sign) == this.newVal.length - 1 ) {
+          this.$message.error(`出错啦,${sign}`)
+        }
         this.result = eval(this.newVal.replace('÷', '/'))
       } else {
         this.result = +this.result
       }
+      // eslint-disable-next-line use-isnan
+      // (this.result === NaN) && this.$message.error('嗨呀，出错啦')
     }
   }
 }
@@ -163,18 +174,26 @@ export default {
 <style scoped>
 .wrapper {
   width: 340px;
-  height: 300px;
   padding: 10px;
-  margin: 6vh auto;
-  border: 1px solid #ccc;
-  background-color: rgb(80, 202, 43);
+  margin: 0 auto 20px;
+  color: #fff;
+  background-color: #333;
   border-radius: 5px;
   text-align: center;
 }
 .title {
-  border-bottom: 1px solid #000;
+  padding: 10px;
 }
 .mb10 {
   margin-bottom: 10px;
+}
+button {
+  width: 94%;
+}
+.operate {
+  background-color: #5A5A5A;
+}
+.num {
+  background-color: #DDDDDD;
 }
 </style>
